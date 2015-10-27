4b399bc3eacebae6af8e97a056c1afacc29bc69b
Xiaowei <wxwwayne@gmail.com>
2015-10-27 15:36:13 -0400

    git init

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,50 @@
+class GroupsController < ApplicationController
+  def index
+    @groups = Group.all
+  end
+
+  def show
+    @group = Group.find(params[:id])
+  end
+
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.create(group_params)
+
+    if @group.save
+      redirect_to groups_url
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @group = Group.find(params[:id])
+    @group.destroy
+    redirect_to groups_url
+    flash[:danger] = "Group has been deleted!"
+  end
+
+  def update
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to groups_url
+      flash[:success] = "Group update successful!"
+    else
+      render 'edit'
+    end
+  end
+
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  private
+  def group_params
+    params.require(:group).permit(:title, :description)
+  end
+
+end
