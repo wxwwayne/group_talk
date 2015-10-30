788418d7602b58c87df4bacb69a506b8bbb575d7
Xiaowei <wxwwayne@gmail.com>
2015-10-30 10:46:43 -0400

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+#users
+User.create!(name: "TestAccount",
+             email: "test@account.com",
+             password: "password",
+             password_confirmation: "password"
+             )
+
+#groups
+20.times do |n|
+  title = Faker::Hacker.adjective
+  description = Faker::Hacker.say_something_smart
+  Group.create!(title: title,
+                description: description,
+                user_id: "8")
+end
+
+#posts
+users = User.order(:created_at).take(3)
+10.times do
+  content = Faker::Lorem.sentence(4)
+  users.each { |user| user.posts.create!(content: content,
+                                         group_id: rand(6...27))}
+end
