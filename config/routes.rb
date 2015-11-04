706525ba23e7e3fb2eb3c5e4b05f08421ff08d8d
Xiaowei <wxwwayne@gmail.com>
2015-11-04 12:55:35 -0500

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'groups#index'
+  root 'api/v1/groups#index'
   namespace :api do
     namespace :v1 do
       resources :groups do
